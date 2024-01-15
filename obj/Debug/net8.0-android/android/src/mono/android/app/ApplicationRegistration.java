a2a4678d39b103af4acb5559b3d5339046fac6a3
david <dtrdic2@gmail.com>
2024-01-15 15:16:45 +0100

    update

---
+++ obj/Debug/net8.0-android/android/src/mono/android/app/ApplicationRegistration.java
@@ -0,0 +1,12 @@
+package mono.android.app;
+
+public class ApplicationRegistration {
+
+	public static void registerApplications ()
+	{
+				// Application and Instrumentation ACWs must be registered first.
+		mono.android.Runtime.register ("MauiNet8.MainApplication, MauiNet8, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", crc6409bd4bd2f8e58266.MainApplication.class, crc6409bd4bd2f8e58266.MainApplication.__md_methods);
+		mono.android.Runtime.register ("Microsoft.Maui.MauiApplication, Microsoft.Maui, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", crc6488302ad6e9e4df1a.MauiApplication.class, crc6488302ad6e9e4df1a.MauiApplication.__md_methods);
+		
+	}
+}
